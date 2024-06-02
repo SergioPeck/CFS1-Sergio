e66fd7dbe699f057b18300006ffa438daee5ab3b
SergioPeck <sergiopeckerle2004@gmail.com>
2024-06-02 15:20:56 -0300

    commit de cosas basicas arregladas

--- cosasBásicas/clase8Arrays/clase8.ts
+++ cosasBásicas/clase8Arrays/clase8.ts
@@ -1,7 +1,7 @@
 import * as readlineSync from "readline-sync"
 
 let Numerito:number=readlineSync.questionInt("\nQue numero de mes quieres? ")
-let NumeroMeses:string[] = new Array(13);
+let NumeroMeses:string[] = new Array(12);
 
 NumeroMeses[0]="Enero";
 NumeroMeses[1]="febrero";
